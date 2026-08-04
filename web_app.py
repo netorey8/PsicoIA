05968dae179fb524b4f05a8e613c4ef67650e999
netorey8 <nestoreyna1234@gmail.com>
2026-08-04 11:55:45 -0500

    Add files via upload

--- web_app.py
+++ web_app.py
@@ -15,6 +15,10 @@ try:
 except ImportError:
     EDGE_TTS_AVAILABLE = False
 
+# Asegurar importaciones locales
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from psychologist import PsychologistBot
+
 from shared_utils import (
     get_appdata_dir,
     get_settings_path,
